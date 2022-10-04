ccd2a430f7889e4eb7cc942d5b0cf9f8379119e1
Peter Butkovic <butkovic@gmail.com>
2022-10-04 08:51:58 +0200

    fix: remove useless annotations

--- cache/src/main/java/com/github/tech_salad/resilience/cache/client/RestClient.java
+++ cache/src/main/java/com/github/tech_salad/resilience/cache/client/RestClient.java
@@ -1,9 +1,6 @@
 package com.github.tech_salad.resilience.cache.client;
 
 import com.github.tech_salad.resilience.cache.model.Drink;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -11,12 +8,9 @@ public interface RestClient<T> {
 
     List<T> getAll();
 
-    @CacheEvict(value="drinks", allEntries=true)
     List<T> saveAll(List<T> drinks);
 
-    @CachePut(value="addresses")
     Drink save(Drink drink);
 
-    @Cacheable(value="#name")
     Drink get(String name);
 }
