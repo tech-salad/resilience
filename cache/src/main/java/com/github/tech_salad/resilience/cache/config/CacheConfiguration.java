54417b9455b678388136c143b01d2bd72e65982a
Peter Butkovic <butkovic@gmail.com>
2022-10-05 20:58:40 +0200

    fix: removed unused imports

--- cache/src/main/java/com/github/tech_salad/resilience/cache/config/CacheConfiguration.java
+++ cache/src/main/java/com/github/tech_salad/resilience/cache/config/CacheConfiguration.java
@@ -1,14 +1,12 @@
 package com.github.tech_salad.resilience.cache.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
